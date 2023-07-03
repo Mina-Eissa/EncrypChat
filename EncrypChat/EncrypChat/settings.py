1b61135838cf66d24148ad9d45ec6f3ccec956fb
Mina-Eissa <minaeissa268@gmail.com>
2023-07-03 05:56:43 +0300

    use mysql database instead of sqllite3

--- EncrypChat/EncrypChat/settings.py
+++ EncrypChat/EncrypChat/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'EncrypChat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'encrypchat',
+        'USER':'Mina',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
