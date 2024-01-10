60b1c709f42a15742c2f57f5f9ce61b316994c5b
Mina-Eissa <minaeissa268@gmail.com>
2024-01-10 21:16:23 +0200

    + add admin user for my site:  - create superuser for my site and register my models in admin site + change the site time zone to egypt

--- EncrypChat/api/admin.py
+++ EncrypChat/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Member, Message, Chat
+
+admin.site.register(Member)
+admin.site.register(Message)
+admin.site.register(Chat)
